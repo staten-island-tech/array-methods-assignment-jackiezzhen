049bcad2209421d7f7d7904e91341256b789d84c
jackiezzhen <jackiezhen31@gmail.com>
2023-10-20 15:34:47 -0400

    tete

--- app.js
+++ app.js
@@ -6,39 +6,32 @@ const iphone = [
         release_date:'September 24, 2021',
         variants:'Pro, Pro Max, Plus',
         feature:['Titanium Frame (Pro/ProMax) Exclusive', 'A16 & A17 Pro Bionic Chip', 'USB-C'],
+        cameras:[3],
     },
     {
         name: 'iPhone 14',
         release_date:'September 16, 2022',
         variants:'Pro, Pro Max, Plus',
         feature: ['A15 & A16 Bionic Chip', 'Dynamic Island', 'Optical Zoom Additions'],
-        
+        cameras:[2,3],
     },
     {
         name: 'iPhone 13',
         release_date:'September 22, 2023',
         variants:'Mini, Pro, Pro Max',
         feature:['A15 Bionic Chip', 'Optical Zoom Additions'],
-        filter: function(){
-            return filtered = this.feature;}
+        cameras:[2,3],
     }
 
 ]
 iphone.forEach((iphone) => {
     console.log(iphone.name);
     console.log(iphone.variants);
-    console.log(iphone.release_date);
+    console.log(iphone.release_date);   
     iphone.feature.forEach((feature) => {
         console.log(feature);
     });
 });
 
-const filtered = iphone.filter((filtered) => filtered.variants.includes('Plus'));
-filtered.forEach((filtered) => {
-    console.log(filtered.name);
-    console.log(filtered.variants);
-    console.log(filtered.release_date);
-    filtered.feature.forEach((feature) => {
-        console.log(feature);
-    });
-});
+const filtered = iphone.filter((iphone) => iphone.cameras.includes(2));
+console.log(filtered);
