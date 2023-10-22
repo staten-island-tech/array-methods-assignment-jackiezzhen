f92573ba7be954e8b4607e38cb3dfc6f6e8beb4c
jackiezzhen <jackiezhen31@gmail.com>
2023-10-22 19:38:28 -0400

    v43534

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
-
-//i have no good topics so why not iphone
 const iphone = [
-    {
-        name: 'iPhone 15',
-        release_date:'September 24, 2021',
-        variants:'Pro, Pro Max, Plus',
-        feature:['Titanium Frame (Pro/ProMax) Exclusive', 'A16 & A17 Pro Bionic Chip', 'USB-C'],
-        cameras:[3],
-    },
-    {
-        name: 'iPhone 14',
-        release_date:'September 16, 2022',
-        variants:'Pro, Pro Max, Plus',
-        feature: ['A15 & A16 Bionic Chip', 'Dynamic Island', 'Optical Zoom Additions'],
-        cameras:[2,3],
-    },
-    {
-        name: 'iPhone 13',
-        release_date:'September 22, 2023',
-        variants:'Mini, Pro, Pro Max',
-        feature:['A15 Bionic Chip', 'Optical Zoom Additions'],
-        cameras:[2,3],
-    }
+  {
+    name: "iPhone 15",
+    release_date: "September 24, 2021",
+    variants: "Pro, Pro Max, Plus",
+    feature: [
+      "Titanium Frame (Pro/ProMax) Exclusive",
+      "A16 & A17 Pro Bionic Chip",
+      "USB-C",
+    ],
+  },
+  {
+    name: "iPhone 14",
+    release_date: "September 16, 2022",
+    variants: "Pro, Pro Max, Plus",
+    feature: [
+      "A15 & A16 Bionic Chip",
+      "Dynamic Island",
+      "Optical Zoom Additions",
+    ],
+  },
+  {
+    name: "iPhone 13",
+    release_date: "September 22, 2023",
+    variants: "Mini, Pro, Pro Max",
+    feature: ["A15 Bionic Chip", "Optical Zoom Additions"],
+  },
+];
 
-]
 iphone.forEach((iphone) => {
-    console.log(iphone.name);
-    console.log(iphone.variants);
-    console.log(iphone.release_date);   
-    iphone.feature.forEach((feature) => {
-        console.log(feature);
-    });
+  console.log(iphone.name);
+  console.log(iphone.variants);
+  console.log(iphone.release_date);
+  iphone.feature.forEach((feature) => {
+    console.log(feature);
+  });
 });
 
-const filtered = iphone.filter((iphone) => iphone.cameras.includes(2));
-console.log(filtered);
+const filteredPhones = iphone.filter((phone) =>
+  phone.variants.includes("Plus")
+);
+console.log(filteredPhones)
--- example.js
+++ example.js
@@ -1,52 +1,52 @@
-
 const students = [
     {
-        firstName: 'eh',
-        lastName: 'eh',
-        graduated: true,
-        DOB:2000,
-        sibings:['eh','bleh'],
-        age: function(){
+        firstName: 'John',
+        lastName: 'Doe',
+        graduated: false,
+        DOB: 2000,
+        siblings: ['Alice', 'Bob'],
+        age: function() {
             return year = this.DOB;
         }
     },
     {
-        firstName: 'eh',
-        lastName: 'eh',
+        firstName: 'Alice',
+        lastName: 'Smith',
         graduated: true,
-        DOB:2000,
-        sibings:['eh','bleh'],
-        age: function(){
+        DOB: 1999,
+        siblings: ['Charlie'],
+        age: function() {
             return year = this.DOB;
         }
     },
     {
-        firstName: 'eh',
-        lastName: 'eh',
+        firstName: 'Bob',
+        lastName: 'Johnson',
         graduated: true,
-        DOB:2000,
-        sibings:['eh','bleh'],
-        age: function(){
+        DOB: 2001,
+        siblings: ['Eva', 'David'],
+        age: function() {
             return year = this.DOB;
         }
     },
     {
-        firstName: 'eh',
-        lastName: 'eh',
-        graduated: true,
-        DOB:2000,
-        sibings:['eh','bleh'],
-        age: function(){
+        firstName: 'Eva',
+        lastName: 'Brown',
+        graduated: false,
+        DOB: 2002,
+        siblings: ['Frank', 'Grace', 'Hannah'],
+        age: function() {
             return year = this.DOB;
         }
     }
-]
+];
+
+students.forEach((student) => console.log(student.firstName));
 
 //for each loop
 //()=> arrow function
 //arrow functions auto return, this key word points globals
 //let nums= [1, 2, 3, 4, 5];
-students.forEach((students) => console.log(students.siblings));
 /*
 nums.forEach(function (e1){
     console.log(e1);
